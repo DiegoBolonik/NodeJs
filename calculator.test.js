09eebac458392c62734e962bd11ce1f6f25f0703
DiegoBolonik <bolonik22@gmail.com>
2021-09-25 20:25:27 -0300

    x5

--- calculator.test.js
+++ calculator.test.js
@@ -4,13 +4,13 @@
 const calculator = require('./calculator');
 
 test("testando soma de 1+1", () => {
-expect(calculator.soma(1,1)).toBe(2);
+    expect(calculator.soma(1,1)).toBe(2);
 }); 
 
 test("testar divisao de 42/6", () => {
-expect(calculator.divisao(42,6)).toBe(7);
+    expect(calculator.divisao(42,6)).toBe(7);
 })
     
 test("testar soma de 5+5", () => {
-expect(calculator.soma(5,5)).toBe(11);
+    expect(calculator.soma(5,5)).toBe(10);
 })
